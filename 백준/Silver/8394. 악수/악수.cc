9dd670f3f6a70cdd32a24e0187366e1f775cadd5
yejin <110565304+yejin7211@users.noreply.github.com>
2022-09-12 15:44:06 +0900

    [Silver III] Title: 악수, Time: 52 ms, Memory: 40172 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int dp[10000001];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 2] + dp[i - 1];
+		dp[i] %= 10;
+	}
+
+	printf("%d", dp[n]);
+
+	return 0;
+}
