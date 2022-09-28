7c4f4357e1bd3690e7ed782aa178b9587bb4491f
yejin <110565304+yejin7211@users.noreply.github.com>
2022-09-29 01:50:16 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -2,29 +2,27 @@
 #include <queue>
 using namespace std;
 
-priority_queue<int, vector<int>, greater<int>> pq;
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	
+	priority_queue<int, vector<int>, greater<int>> pq;
 
-    int n;
+	int n;
 	cin >> n;
 
 	while (n--) {
 		int x;
 		cin >> x;
-
 		if (x == 0) {
-			if (pq.empty()) cout << 0 << "\n";
+			if (pq.empty()) cout << 0 << '\n';
 			else {
-				cout << pq.top() << "\n";
+				cout << pq.top() << '\n';
 				pq.pop();
 			}
 		}
 		else pq.push(x);
 	}
 	return 0;
-
 }
