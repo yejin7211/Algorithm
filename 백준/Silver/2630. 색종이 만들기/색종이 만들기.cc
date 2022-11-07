52856855bc2ed4129be30cc030de899d9d612e9c
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2022-11-07 23:16:39 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,43 +1,35 @@
 #include <iostream>
-#include <cstdbool>
 
 using namespace std;
 
-int map[128][128];
-int colorCnt[2];
-int paperCnt[2];
 int n;
+int map[128][128];
+int cnt[2]; // 햐얀색, 파란색 색종이의 개수
 
-// 해당 범위내 색깔이 모두 color와 같은지 확인
-bool isPossible(int r, int c, int len, int color) {
-	for (int i = r; i < r + len; i++)
-		for (int j = c; j < c + len; j++)
-			if (map[i][j] != color)
-				return false;
-
-	return true;
+void input() {
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> map[i][j];
 }
 
-void divideArea(int r, int c, int len, int color) {
-	if (len == 1) { // 1개짜리면
-		if (map[r][c] == color) { //색이 같다면 색종이+1 && 색깔 수-1
-			colorCnt[color]--;
-			paperCnt[color]++;
-		}
-	}
-	else {
-		int check[2] = { 0,len / 2 };
+// 전체 종이가 모두 같은 색으로 칠해져 있는지 확인하는 함수
+int isAllSame(int y, int x, int size) {
+	for (int i = y; i < y + size; i++)
+		for (int j = x; j < x + size; j++)
+			if (map[i][j] != map[y][x])
+				return 0;
+	return 1;
+}
 
-		for (int i = 0; i < 2; i++) {  // 4등분
-			for (int j = 0; j < 2; j++) {
-				if (isPossible(r + check[i], c + check[j], len / 2, color)) { //색이 같다면
-					colorCnt[color] -= (len / 2) * (len / 2);
-					paperCnt[color]++;
-					if (colorCnt[color] == 0) return; //모든 해당 color 확인시 돌아가기
-				}
-				else divideArea(r + check[i], c + check[j], len / 2, color); //아니라면 다시 4등분
-			}
-		}
+void sol(int y, int x, int size) {
+	if (isAllSame(y, x, size))  // 전체 종이가 모두 같은 색으로 칠해져 있는지 확인
+		cnt[map[y][x]]++; // 모두 같은 색이라면 해당 색종이의 개수 세기
+	else { // 색종이 4등분
+		sol(y, x, size / 2);
+		sol(y, x + size / 2, size / 2);
+		sol(y + size / 2, x, size / 2);
+		sol(y + size / 2, x + size / 2, size / 2);
 	}
 }
 
@@ -46,19 +38,8 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	// 입력
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> map[i][j];
-			colorCnt[map[i][j]]++; //흰색과 파랑색 세기
-		}
-	}
-
-	for (int i = 0; i < 2; i++) {
-		if (isPossible(0, 0, n, i)) paperCnt[i]++; //모두 해당색으로 같다면 색종이+1
-		else divideArea(0, 0, n, i); // 그렇지 않다면 4등분
-		cout << paperCnt[i] << '\n'; // 색종이 수 출력
-	}
+	input();
+	sol(0, 0, n);
+	cout << cnt[0] << '\n' << cnt[1];
 	return 0;
-} 
+}
