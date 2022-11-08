d885d76d5bbce226d28b8437d5cbeff313afb517
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2022-11-08 21:14:49 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1452 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -3,34 +3,40 @@
 
 using namespace std;
 
+/*
+multiset
+- 내장함수: set
+- set: 중복을 없앰. 입력 시 정렬이 되며 입력됨
+- set과 비슷하나, multiset은 중복을 허용
+- 기본적으로 오름차순 정렬, greater로 내림차순 정렬 가능
+- intert, clear, begin(), end(), count(n), size()
+*/
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int t;
+	int t, k, n;
+	char c;
 	cin >> t;
 	while (t--) {
-		int k;
+		multiset<int> ms; // default: 오름차순 정렬
 		cin >> k;
-
-		multiset<int> ms;
 		while (k--) {
-			char c;
-			int n;
 			cin >> c >> n;
-			if (c == 'I')
+			if (c == 'I') 
 				ms.insert(n);
 			else {
-				if (!ms.empty() && n == 1) {
-					auto iter = ms.end();
-					ms.erase(--iter);
+				if (ms.empty())
+					continue;
+				if (n == -1) ms.erase(ms.begin()); // 가장 앞 위치 삭제
+				else {
+					auto iter = ms.end(); 
+					ms.erase(--iter); // 가장 뒤 원소가 있는 위치 삭제
 				}
-				else if (!ms.empty() && n == -1) 
-					ms.erase(ms.begin());
 			}
 		}
-
 		if (ms.empty()) cout << "EMPTY" << '\n';
 		else {
 			auto iter = ms.end();
