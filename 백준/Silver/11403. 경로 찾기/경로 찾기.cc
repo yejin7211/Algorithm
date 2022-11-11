44643be5f9f4baedaf45946896c9732c871bb9b9
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2022-11-11 16:11:11 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 using namespace std;
 
-int map[101][101];
 int n;
+int map[101][101];
+int visited[101];
 
-void Floyd() {
-	for (int k = 1; k <= n; k++) {
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (map[i][k] == 1 && map[k][j] == 1)
-					map[i][j] = 1;
-			}
+void dfs(int v) {
+	for (int w = 1; w <= n; w++) {
+		if (map[v][w] == 1 && !visited[w]) {
+			visited[w] = 1;
+			dfs(w);
 		}
 	}
 }
@@ -21,19 +20,18 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) 
-			cin >> map[i][j];
-	}
-
-	Floyd();
-
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++)
-			cout << map[i][j] << ' ';
+			cin >> map[i][j];
+
+	for (int i = 1; i <= n; i++) {
+		dfs(i);
+		for (int j = 1; j <= n; j++) 
+			cout << visited[j] << ' ';
 		cout << '\n';
+		memset(visited, 0, sizeof(visited));
 	}
 	return 0;
 }
