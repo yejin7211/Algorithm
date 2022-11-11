800187242fc1519fb65b1b4994127fe7f1a407ef
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2022-11-11 15:53:26 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,57 +1,98 @@
 #include <iostream>
-#include <cstdbool>
+#include <cstring>
 #include <queue>
 #include <tuple>
 
 using namespace std;
 
-queue<tuple<int,int,int>> q; // 아기 상어의 y좌표, x좌표, 움직임 횟수
-int map[20][20];
-bool visited[20][20];
-int ypos, xpos, sharkSize = 2, eatenFish = 0; //아기 상어의 위치, 크기, 먹은 물고기 수
 int n;
+int map[20][20];
+int sharkY, sharkX, sharkSize = 2;
+int eatenCnt = 0;
+queue<tuple<int, int, int>> q; 
+int movedT, movedY, movedX;
+int t = 0;
 
-int dy[4] = { -1,1,0,0 };
-int dx[4] = { 0,0,-1,1 };
-
-/*
-* 먹을 수 있는 물고기의 각각 거리 찾기
-* 아기 상어가 가장 가까운 물고기에게 이동
-*/
-
-void reset() {
-	q = queue<tuple<int, int, int>>();
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			visited[i][j] = false;
-}
-
-int bfs(int goal_y, int goal_x) {
-	q.push({ ypos,xpos,0 });
-	visited[ypos][xpos] = true;
-
-	while (!q.empty()) {
-		int y = get<0>(q.front());
-		int x = get<1>(q.front());
-		int moved = get<2>(q.front());
-		q.pop();
-
-		if (y == goal_y && x == goal_x) 
-			return moved;
-
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-
-			if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;		
-			//크기가 같은 물고기는 먹을 수 없지만, 해당 칸은 지나갈 수 있다.
-			if (!visited[ny][nx] && map[ny][nx] <= sharkSize) {
-				q.push({ ny,nx,moved + 1 });
-				visited[ny][nx] = true;
+void input() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 9) {
+				sharkY = i;
+				sharkX = j;
 			}
 		}
 	}
-	return -1;
+}
+
+void search_eatableFish() {
+	int visited[20][20];
+	memset(visited, 0, sizeof(visited));
+	q = queue<tuple<int, int, int>>();
+	int dy[4] = { -1,1,0,0 };
+	int dx[4] = { 0,0,-1,1 };
+
+	// 이동 시간, 아기상어의 y좌표, 아기상어의 x좌표
+	q.push({ 0, sharkY, sharkX });
+	visited[sharkY][sharkX] = 1;
+	while (!q.empty()) {
+		int d = get<0>(q.front());
+		int y = get<1>(q.front());
+		int x = get<2>(q.front());
+		q.pop();
+		if (map[y][x] != 9 && map[y][x] != 0 && map[y][x] < sharkSize) {
+			if (d < movedT) {
+				movedT = d;
+				movedY = y;
+				movedX = x;
+			}
+			else if (d == movedT) {
+				if (y < movedY) {
+					movedT = d;
+					movedY = y;
+					movedX = x;
+				}
+				else if (y == movedY) {
+					if (x < movedX) {
+						movedT = d;
+						movedY = y;
+						movedX = x;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+			if (!visited[ny][nx] && map[ny][nx] <= sharkSize) {
+				q.push({ d + 1,ny,nx });
+				visited[ny][nx] = 1;
+			}
+		}
+	}
+}
+
+void sol() {
+	while (1) {
+		movedT = 987654321;
+		search_eatableFish();
+		if (movedT == 987654321)
+			break;
+
+		map[sharkY][sharkX] = 0;
+		sharkY = movedY;
+		sharkX = movedX;
+		map[sharkY][sharkX] = 9;
+		t += movedT;
+		eatenCnt++;
+		if (eatenCnt == sharkSize) {
+			sharkSize++;
+			eatenCnt = 0;
+		}
+	}
+	cout << t;
 }
 
 int main() {
@@ -59,54 +100,7 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	// 입력
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 9) {
-				ypos = i;
-				xpos = j;
-			}
-		}
-	}
-
-	int totalT = 0;
-	while (1) {
-		int minT = 987654321;
-		int new_ypos = ypos, new_xpos = xpos;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[i][j] != 9 && map[i][j] != 0 && map[i][j] < sharkSize) { //먹을 수 있는 물고기 발견
-					int t = bfs(i, j); // 먹을 수 있는 물고기와의 거리 구하기
-					if (t!= -1 && minT > t) {
-						minT = t; 
-						new_ypos = i; 
-						new_xpos = j; 
-					}
-					reset(); 
-				}
-			}
-		}
-		
-		//더 이상 먹을 수 있는 물고기가 없다면, 엄마 상어에게 도움을 요청한다.
-		if (minT == 987654321) 
-			break; 
-		
-		//먹을 수 있는 물고기 중 거리가 가장 가까운 물고기를 먹으러 간다.
-		totalT += minT;  
-		map[ypos][xpos] = 0;
-		ypos = new_ypos;  
-		xpos = new_xpos;
-		map[ypos][xpos] = 9;
-		eatenFish++;
-
-		//아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1 증가한다.
-		if (sharkSize == eatenFish) {
-			eatenFish = 0;
-			sharkSize++;
-		}
-	}
-	cout << totalT;
+	input();
+	sol();
 	return 0;
 }
