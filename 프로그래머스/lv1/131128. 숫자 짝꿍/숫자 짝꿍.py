bdb679b26799e72e32e47bfa7f364862b64a958c
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2022-11-12 21:56:16 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,25 @@
+def solution(X, Y):
+    answer = ""
+    listX = list(''.join(X))
+    listY = list(''.join(Y))
+    numsDic = {}
+    for c in listY:
+        if c in numsDic:
+            numsDic[c] += 1
+        else:
+            numsDic[c] = 1
+            
+    listXY = list()
+    for c in listX:
+        if c in numsDic and numsDic[c] > 0:
+            listXY.append(c)
+            numsDic[c] -= 1
+    if listXY == []:
+        return "-1"
+
+    listXY.sort(reverse=True)
+    for v in listXY:
+        answer += v
+    if answer[0] == "0" and "00" in answer:
+        return "0"
+    return answer
