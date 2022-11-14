f90936b8ae08e7c512ab5f25e7d09897a84f2fa7
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2022-11-14 22:49:22 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,26 +1,25 @@
 def solution(dartResult):
-    dartResult = dartResult.replace('10', 'A')
     scores = {'S':1, 'D':2, 'T':3}
     options = {'*':2, '#':-1}
     
     nums = []
-    numsIdx = -1
+    nIdx = -1
     
     i = 0
     while i < len(dartResult):
-        if dartResult[i] == 'A': # 10
-            numsIdx += 1
-            nums.append(10)
-        elif dartResult[i].isdigit(): # 정수
-            numsIdx += 1
-            nums.append(int(dartResult[i]))
-        else:
-            if dartResult[i] in ['S', 'D', 'T']: # S, D, T
-                nums[numsIdx] **= scores[dartResult[i]]
-            elif dartResult[i] in ['*', '#']:  # *, #
-                nums[numsIdx] *= options[dartResult[i]]
-                if numsIdx != 0 and dartResult[i] == '*': # 처음이 아니라면 그 이전 수도 *2
-                    nums[numsIdx - 1] *= 2
+        if dartResult[i].isdigit():
+            nIdx += 1
+            if i + 1 < len(dartResult) and dartResult[i+1].isdigit():
+                nums.append(10)
+                i += 1
+            else:
+                nums.append(int(dartResult[i]))
+        elif dartResult[i] in ['S', 'D', 'T']: # S, D, T
+            nums[nIdx] **= scores[dartResult[i]]
+        elif dartResult[i] in ['*', '#']:  # *, #
+            nums[nIdx] *= options[dartResult[i]]
+            if nIdx != 0 and dartResult[i] == '*': # 처음이 아니라면 그 이전 수도 *2
+                nums[nIdx - 1] *= 2
         i += 1
-            
+
     return sum(nums)
