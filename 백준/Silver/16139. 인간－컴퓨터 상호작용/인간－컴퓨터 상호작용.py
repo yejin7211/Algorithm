8371ed21f7e57f95a064ca333279c6e6857f520d
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-01-17 19:11:07 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 160 ms, Memory: 121200 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -2,7 +2,18 @@ import sys
 input = sys.stdin.readline
 
 s = input().rstrip()
+alpha = [{chr(c): 0 for c in range(97, 123)} for _ in range(len(s))]
+for i in range(len(s)):
+    alpha[i][s[i]] += 1
+for i in range(1, len(s)):
+    for j in range(97, 123):
+        alpha[i][chr(j)] += alpha[i-1][chr(j)]
+
 q = int(input())
 for _ in range(q):
     c, l, r = input().split()
-    print(s[int(l):int(r)+1].count(c))
+    n = alpha[int(r)][c]
+    m = 0
+    if int(l)-1 >= 0:
+        m = alpha[int(l)-1][c]
+    print(n - m)
