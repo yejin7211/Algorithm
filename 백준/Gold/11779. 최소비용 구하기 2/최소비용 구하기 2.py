3d3863b1c7f76637220c035f8f4e98315372d513
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-01-20 19:32:19 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 384 ms, Memory: 151872 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,29 @@
+import sys
+from heapq import heappush, heappop
+from collections import defaultdict
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+graph = defaultdict(list)
+for _ in range(m):
+    s, e, w = map(int, input().split())
+    graph[s].append([e, w])
+s, e = map(int, input().split())
+
+dist = [float('inf') for _ in range(n+1)]
+heap = [[0, s, [s]]]
+dist[s] = 0
+while heap:
+    cost, cur, visited = heappop(heap)
+    if cost > dist[cur]:
+        continue
+    if cur == e:
+        print(cost)
+        print(len(visited))
+        print(*visited)
+        break
+    for pos, wei in graph[cur]:
+        if dist[cur] + wei < dist[pos]:
+            dist[pos] = dist[cur] + wei
+            heappush(heap, [dist[pos], pos, visited+[pos]])
