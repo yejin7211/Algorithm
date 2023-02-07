251d053ad0861b72849c2a311d3d3c25ea8c5134
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-02-07 20:25:48 +0900

    [Gold IV] Title: 네트워크 연결, Time: 340 ms, Memory: 130628 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,20 +1,29 @@
 import sys
 input = sys.stdin.readline
 
+answer = 0
 n = int(input())
 m = int(input())
-connect = [list(map(int, input().split())) for _ in range(m)]
-connect.sort(key=lambda x: x[2])
+cost = [list(map(int, input().split())) for _ in range(m)]
+cost.sort(key=lambda x: x[2])
+root = [i for i in range(n+1)]
 
-answer = 0
-visited = {connect[0][0]}
-while len(visited) != n:
-    for a, b, c in connect:
-        if a in visited and b in visited:
-            continue
-        if a in visited or b in visited:
-            visited |= {a, b}
-            answer += c
-            break
+
+def find(x):
+    if x != root[x]:
+        root[x] = find(root[x])
+    return root[x]
+
+
+def union(x, y):
+    root[max(x, y)] = min(x, y)
+
+
+for a, b, c in cost:
+    a_root = find(a)
+    b_root = find(b)
+    if a_root != b_root:
+        union(a_root, b_root)
+        answer += c
+
 print(answer)
-
