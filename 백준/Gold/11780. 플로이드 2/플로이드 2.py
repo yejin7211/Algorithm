0b6317fbffa6f29d41b8a874844ddb7ad71ef31e
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-02-07 21:13:06 +0900

    [Gold II] Title: 플로이드 2, Time: 300 ms, Memory: 124816 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.py
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.py
@@ -3,25 +3,29 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-paths = [[[] for _ in range(n+1)] for _ in range(n+1)]
-board = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
+board = [[int(2e9) for _ in range(n+1)] for _ in range(n+1)]
+path = [[[] for _ in range(n+1)] for _ in range(n+1)]
 for _ in range(m):
     a, b, c = map(int, input().split())
-    paths[a][b] = [a, b]
     board[a][b] = min(board[a][b], c)
+    path[a][b] = [a, b]
+
+for i in range(1, n+1):
+    board[i][i] = 0
 
 for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
-            if i != j and board[i][j] > board[i][k] + board[k][j]:
+            if board[i][j] > board[i][k] + board[k][j]:
                 board[i][j] = board[i][k] + board[k][j]
-                paths[i][j] = paths[i][k][:-1] + paths[k][j]
+                path[i][j] = path[i][k][:-1] + path[k][j]
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        print(board[i][j] if board[i][j] != float('inf') else 0, end=' ')
+        print(board[i][j] if board[i][j] != int(2e9) else 0, end=' ')
     print()
+
 for i in range(1, n+1):
     for j in range(1, n+1):
-        print(len(paths[i][j]), end=' ')
-        print(*paths[i][j])
+        print(len(path[i][j]), end=' ')
+        print(*path[i][j])
