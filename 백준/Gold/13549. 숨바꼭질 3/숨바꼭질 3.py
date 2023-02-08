c24a4475d8f9aacaa049ce492680c705652f3012
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-02-08 15:31:58 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 188 ms, Memory: 118416 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,24 +1,29 @@
 import sys
-from heapq import heappush, heappop
+from collections import deque
 input = sys.stdin.readline
 
+MAX = 100001
 n, k = map(int, input().split())
-visited = [False for _ in range(max(n*2, k*2))]
+visited = [False for _ in range(MAX)]
+dist = [int(2e9) for _ in range(MAX)]
 
-heap = [[0, n]]
+q = deque([n])
 visited[n] = True
-while heap:
-    Time, cur = heappop(heap)
-    if cur == k:
-        print(Time)
-        break
-
-    if cur * 2 <= k*2 and not visited[cur*2]:
+dist[n] = 0
+while q:
+    cur = q.popleft()
+    if cur*2 < MAX and not visited[cur*2]:
         visited[cur*2] = True
-        heappush(heap, [Time, cur*2])
+        q.appendleft(cur*2)
+        dist[cur*2] = dist[cur]
+    if cur+1 < MAX and not visited[cur+1]:
+        visited[cur+1] = True
+        q.append(cur+1)
+        dist[cur+1] = dist[cur] + 1
     if cur-1 >= 0 and not visited[cur-1]:
         visited[cur-1] = True
-        heappush(heap, [Time+1, cur-1])
-    if cur+1 <= k*2 and not visited[cur+1]:
-        visited[cur+1] = True
-        heappush(heap, [Time+1, cur+1])
+        q.append(cur-1)
+        dist[cur-1] = dist[cur] + 1
+
+print(dist[k])
+
