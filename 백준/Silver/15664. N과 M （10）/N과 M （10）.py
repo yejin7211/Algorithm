eaa9916603263c5ac5aeebe53d586e9c1821c3ca
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-02-11 17:26:45 +0900

    [Silver II] Title: N과 M (10), Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+
+def dfs(idx, cnt):
+    global res
+    if cnt == m:
+        res2 = res.strip()
+        if res2 not in answer:
+            print(res2)
+            answer.append(res2)
+        return
+
+    for i in range(idx, n):
+        res += arr[i]
+        dfs(i+1, cnt+1)
+        res = res[:-len(arr[i])]
+
+
+answer = []
+n, m = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+arr = [str(num)+' ' for num in arr]
+res = ''
+
+dfs(0, 0)
