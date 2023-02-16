c4d0470cbfceab73418c7e11df646d8b3875ec5d
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-02-16 19:20:32 +0900

    [unrated] Title: 미로 탈출, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+def solution(maps):
+    n, m = len(maps), len(maps[0])
+    visited = [[[False for _ in range(2)] for _ in range(m)] for _ in range(n)]
+    
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
+    
+    q = deque()
+    end_y, end_x = -1, -1
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] == 'S':
+                q.append((i, j, 0, 0))
+                visited[i][j][0] = True
+            if maps[i][j] == 'E':
+                end_y, end_x = i, j
+                
+    while q:
+        y, x, k, time = q.popleft()
+        if y == end_y and x == end_x and k == 1:
+            return time
+            
+        for i in range(4):
+            ny, nx = y+dy[i], x+dx[i]
+            if ny < 0 or ny >= n or nx < 0 or nx >= m:
+                continue
+            if maps[ny][nx] != 'X':
+                if maps[ny][nx] == 'L':
+                    if not visited[ny][nx][1]:
+                        visited[ny][nx][1] = True
+                        q.append((ny, nx, 1, time+1))
+                elif not visited[ny][nx][k]:
+                    visited[ny][nx][k] = True
+                    q.append((ny, nx, k, time+1))
+    
+    return -1
