607194cbf13b5d2b7046f555bfc677f0755a8107
Yejin Cho <110565304+yejin7211@users.noreply.github.com>
2023-05-19 11:30:58 +0900

    [Gold I] Title: 최솟값, Time: 1564 ms, Memory: 38324 KB -BaekjoonHub

--- 백준/Gold/10868. 최솟값/최솟값.py
+++ 백준/Gold/10868. 최솟값/최솟값.py
@@ -13,19 +13,19 @@ def init(start, end, idx):
 
 def interval_min(start, end, idx, left, right):
     if end < left or right < start:
-        return int(2e9)
+        return INF
     if left <= start and end <= right:
         return tree[idx]
     mid = (start + end) // 2
     return min(interval_min(start, mid, idx*2, left, right), interval_min(mid+1, end, idx*2+1, left, right))
 
 
+INF = int(2e9)
 n, m = map(int, input().split())
-arr = [int(input()) for _ in range(n)]
-tree = [int(2e9) for _ in range(n * 4)]
+arr = list(int(input()) for _ in range(n))
+tree = [INF for _ in range(n * 4)]
 init(0, n-1, 1)
 
 for _ in range(m):
     a, b = map(int, input().split())
     print(interval_min(0, n-1, 1, a-1, b-1))
-
